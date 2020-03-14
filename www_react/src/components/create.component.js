ba7795a7ff9445f65a4635a023a605e50eaa1d29
IBM thinkpad <jonathan.giacomelli@ibm.com>
2020-03-14 13:35:18 -0500

    aspects and aptitudes follow game logic, still missing XP calc and form error alerts

--- www_react/src/components/create.component.js
+++ www_react/src/components/create.component.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 export default class Create extends Component {
   constructor(props) {
     super(props);
+    this.checkXP = this.checkXP.bind(this);
+    this.checkAspects = this.checkAspects.bind(this);
+    this.checkAptitudes = this.checkAptitudes.bind(this);
     this.onChangeName = this.onChangeName.bind(this);
     this.onChangeOwner = this.onChangeOwner.bind(this);
     this.onChangeConcept = this.onChangeConcept.bind(this);
@@ -59,6 +62,13 @@ export default class Create extends Component {
 		vice: '',
 		racial: '',
 		description: '',
+		physical: 0,
+		mental: 0,
+		social: 0,
+		aspect_largest_msg: '',
+		aspect_mid_msg: '',
+		aspect_smallest_msg: '',
+		aptitude_total: 20,
 		intelligence: 1,
 		wits: 1,
 		resolve: 1,
@@ -91,13 +101,90 @@ export default class Create extends Component {
 		astrylose: 0,
 		willpower: 0,
 		vitality: 0,
-		size: 0,
+		size: 5,
 		speed: 0,
 		initiative: 0,
 		defense: 0,
+		available_xp: 15,
 		temp_text_box: ''
     }
   }
+
+  checkXP(){
+
+  }
+
+  checkAspects(){
+    this.setState({
+      physical: parseInt(this.state.strength)  + parseInt(this.state.dexterity)  + parseInt(this.state.stamina) -3, 
+      mental: parseInt(this.state.intelligence)  + parseInt(this.state.wits)  + parseInt(this.state.resolve) -3, 
+      social: parseInt(this.state.presence)  + parseInt(this.state.manipulation)  + parseInt(this.state.composure) -3
+    }, () => {
+	    var array = [{key: 'physical', value: this.state.physical}, 
+	    			{key: 'mental', value: this.state.mental}, 
+	    			{key: 'social', value: this.state.social} 
+	    			];
+	    array.sort(function(obj1, obj2) {
+		   return obj1.value - obj2.value;
+		});
+		console.log("smallest element? ", array[0].key, array[0].value, );
+		console.log("mid element? ", array[1].key, array[1].value, );
+		console.log("largest element? ", array[2].key, array[2].value, );
+		if(array[2].value == 5){
+			this.setState({aspect_largest_msg: "Largest is "+array[2].key+" and at 5."});
+		}else if (array[2].value > 5){
+			this.setState({aspect_largest_msg: "Largest is "+array[2].key+" and is above 5. This will incur XP cost or maybe need reworking."});
+		}else{ // less than 5
+			this.setState({aspect_largest_msg: "Largest is "+array[2].key+" and the allowed amount (5). Feel free to add "+(5-array[2].value)+" more dots!"});
+		}
+
+		if(array[1].value == 4){
+			this.setState({aspect_mid_msg: "Mid is "+array[1].key+" and at 4."});
+		}else if (array[1].value > 4){
+			this.setState({aspect_mid_msg: "Mid is "+array[1].key+" and is above 4. This will incur XP cost or maybe need reworking."});
+		}else{ // less than 4
+			this.setState({aspect_mid_msg: "Mid is "+array[1].key+" and the allowed amount (4). Feel free to add "+(4-array[1].value)+" more dots!"});
+		}
+
+		if(array[0].value == 3){
+			this.setState({aspect_smallest_msg: "Smallest is "+array[0].key+" and at 3."});
+		}else if (array[0].value > 3){
+			this.setState({aspect_smallest_msg: "Smallest is "+array[0].key+" and is above 3. This will incur XP cost or maybe need reworking."});
+		}else{ // less than 3
+			this.setState({aspect_smallest_msg: "Smallest is "+array[0].key+" and the allowed amount (3). Feel free to add "+(3-array[0].value)+" more dots!"});
+		}
+
+	});
+  }
+
+  checkAptitudes(){
+    this.setState({
+      aptitude_total: 23 
+      				- parseInt(this.state.athletics)
+      				- parseInt(this.state.crafts)
+      				- parseInt(this.state.culture)
+      				- parseInt(this.state.empathy)
+      				- parseInt(this.state.expression)
+      				- parseInt(this.state.intimidation)
+      				- parseInt(this.state.investigation)
+      				- parseInt(this.state.larceny)
+      				- parseInt(this.state.luck)
+      				- parseInt(this.state.magicka)
+      				- parseInt(this.state.medicine)
+      				- parseInt(this.state.observation)
+      				- parseInt(this.state.persuasion)
+      				- parseInt(this.state.portaelogy)
+      				- parseInt(this.state.riding)
+      				- parseInt(this.state.stealth)
+      				- parseInt(this.state.streetwise)
+      				- parseInt(this.state.stealth)
+      				- parseInt(this.state.streetwise)
+      				- parseInt(this.state.subterfuge)
+      				- parseInt(this.state.survival)
+      				- parseInt(this.state.technika)
+    });
+  }
+
   onChangeName(e) {
     this.setState({
       name: e.target.value
@@ -116,6 +203,7 @@ export default class Create extends Component {
     }) 
   }
   onChangeConcept(e) {
+  	console.log("onChangeConcept", e.currentTarget);
     this.setState({
       concept: e.target.value
     });
@@ -143,26 +231,34 @@ export default class Create extends Component {
   onChangeIntelligence(e) {
     this.setState({
       intelligence: e.target.value
-    });
+    }, () => {
+	    this.checkAspects();
+	});
   }
   onChangeWits(e) {
     this.setState({
       wits: e.target.value,
       defense: Math.min(e.target.value , this.state.dexterity)
-    });
+    }, () => {
+	    this.checkAspects();
+	});
   }
   onChangeResolve(e) {
     this.setState({
       resolve: e.target.value,
       astrylose: parseInt(e.target.value) + parseInt(this.state.size),
       willpower: parseInt(e.target.value) + parseInt(this.state.composure)
-    });
+    }, () => {
+	    this.checkAspects();
+	});
   }
   onChangeStrength(e) {
     this.setState({
       strength: e.target.value,
       speed: parseInt(e.target.value) + parseInt(this.state.dexterity) + parseInt(this.state.size)
-    });
+    }, () => {
+	    this.checkAspects();
+	});
   }
   onChangeDexterity(e) {
     this.setState({
@@ -170,35 +266,47 @@ export default class Create extends Component {
       speed: parseInt(e.target.value) + parseInt(this.state.strength) + parseInt(this.state.size),
       initiative: parseInt(e.target.value) + parseInt(this.state.composure),
       defense: Math.min(e.target.value , this.state.wits)
-    });
+    }, () => {
+	    this.checkAspects();
+	});
   }
   onChangeStamina(e) {
     this.setState({
       stamina: e.target.value,
       vitality: parseInt(e.target.value) + parseInt(this.state.size)
-    });
+    }, () => {
+	    this.checkAspects();
+	});
   }
   onChangePresence(e) {
     this.setState({
       presence: e.target.value
-    });
+    }, () => {
+	    this.checkAspects();
+	});
   }
   onChangeManipulation(e) {
     this.setState({
       manipulation: e.target.value
-    });
+    }, () => {
+	    this.checkAspects();
+	});
   }
   onChangeComposure(e) {
     this.setState({
       composure: e.target.value,
       willpower: parseInt(e.target.value) + parseInt(this.state.resolve),
       initiative: parseInt(e.target.value) + parseInt(this.state.dexterity)
-    });
+    }, () => {
+	    this.checkAspects();
+	});
   }
   onChangeAthletics(e) {
     this.setState({
       athletics: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeCrafts(e) {
     this.setState({
@@ -208,92 +316,128 @@ export default class Create extends Component {
   onChangeCulture(e) {
     this.setState({
       culture: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeEmpathy(e) {
     this.setState({
       empathy: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeExpression(e) {
     this.setState({
       expression: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeIntimidation(e) {
     this.setState({
       intimidation: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeInvestigation(e) {
     this.setState({
       investigation: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeLarceny(e) {
     this.setState({
       larceny: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeLuck(e) {
     this.setState({
       luck: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeMagicka(e) {
     this.setState({
       magicka: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeMedicine(e) {
     this.setState({
       medicine: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeObservation(e) {
     this.setState({
       observation: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangePersuasion(e) {
     this.setState({
       persuasion: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangePortaelogy(e) {
     this.setState({
       portaelogy: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeRiding(e) {
     this.setState({
       riding: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeStealth(e) {
     this.setState({
       stealth: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeStreetwise(e) {
     this.setState({
       streetwise: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeSubterfuge(e) {
     this.setState({
       subterfuge: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeSurvival(e) {
     this.setState({
       survival: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeTechnika(e) {
     this.setState({
       technika: e.target.value
-    });
+    }, () => {
+	    this.checkAptitudes();
+	});
   }
   onChangeAstrylose(e) {
     // this.setState({
@@ -404,6 +548,10 @@ export default class Create extends Component {
 		vice: '',
 		racial: '',
 		description: '',
+	    physical: 0,
+	    mental: 0,
+	    social: 0,
+	    aptitude_total: 20,
 		intelligence: 1,
 		wits: 1,
 		resolve: 1,
@@ -436,7 +584,7 @@ export default class Create extends Component {
 		astrylose: 0,
 		willpower: 0,
 		vitality: 0,
-		size: 0,
+		size: 5,
 		speed: 0,
 		initiative: 0,
 		defense: 0,
@@ -477,7 +625,7 @@ export default class Create extends Component {
             <h3 align="center">Add New Character Sheet</h3>
             <form onSubmit={this.onSubmit}>
 
-            	<article class="tabs">
+            	<article className="tabs">
 
 					<section id="tab1">
 						<h2><a href="#tab1">Character Desc</a></h2>
@@ -496,6 +644,7 @@ export default class Create extends Component {
 		                    <input 
 		                      type="text" 
 		                      className="form-control" 
+		                      name="concept"
 		                      value={this.state.concept}
 		                      onChange={this.onChangeConcept}
 		                      />
@@ -542,6 +691,10 @@ export default class Create extends Component {
 					<section id="tab2">
 						<h2><a href="#tab2">Aspects</a></h2>
 						<p> Use 5|4|3 rule in the coda to select your Aspects.</p>
+						<p> Currently spent: Physical: {this.state.physical} Mental: {this.state.mental} Social: {this.state.social} </p>
+						<p>{this.state.aspect_largest_msg}</p>
+						<p>{this.state.aspect_mid_msg}</p>
+						<p>{this.state.aspect_smallest_msg}</p>
 						<div className="form-group">
 		                    <label>Intelligence: {this.state.intelligence}</label>
 		                    <input type="range" 
@@ -623,12 +776,12 @@ export default class Create extends Component {
 		                      onChange={this.onChangeComposure}
 		                      />
 		                </div>
-						<p class="tabnav"><a href="#tab3">next &#10151;</a></p>
+						<p className="tabnav"><a href="#tab3">next &#10151;</a></p>
 					</section>
 					
 					<section id="tab3">
 						<h2><a href="#tab3">Aptitudes</a></h2>
-						<p> You get 20 points to spend here, on top of the items already at +1. </p>
+						<p> You get {this.state.aptitude_total} points to spend here, on top of the items already at +1. </p>
 						<div className="form-group">
 		                    <label>Athletics: {this.state.athletics}</label>
 		                    <input type="range" 
@@ -809,7 +962,7 @@ export default class Create extends Component {
 		                      onChange={this.onChangeTechnika}
 		                      />
 		                </div>
-		                <p class="tabnav"><a href="#tab4">next &#10151;</a></p>
+		                <p className="tabnav"><a href="#tab4">next &#10151;</a></p>
 					</section>
 
 
@@ -883,7 +1036,7 @@ export default class Create extends Component {
 		                      />
 		                </div>
 						
-		                <p class="tabnav"><a href="#tab5">next &#10151;</a></p>
+		                <p className="tabnav"><a href="#tab5">next &#10151;</a></p>
 					</section>
 
 
@@ -891,7 +1044,7 @@ export default class Create extends Component {
 					<section id="tab5">
 						<h2><a href="#tab5">Specialties</a></h2>
 						<p>The feature is Coming SOON(tm)!</p>
-						<p class="tabnav"><a href="#tab6">next &#10151;</a></p>
+						<p className="tabnav"><a href="#tab6">next &#10151;</a></p>
 					</section>
 
 
